a232de6b7f089aae08f37e1a24f171baede0e4f8
Yuri Frezzato Espinosa <yurifrezzato@gmail.com>
2024-12-13 19:54:36 +0100

    do not allow 0 division

--- app/api.py
+++ app/api.py
@@ -44,4 +44,4 @@ def divide(op_1, op_2):
         num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
         return ("{}".format(CALCULATOR.divide(num_1, num_2)), http.client.OK, HEADERS)
     except TypeError as e:
-        return (str(e), http.client.BAD_REQUEST, HEADERS)
+        return (str(e), http.client.NOT_ACCEPTABLE, HEADERS)
