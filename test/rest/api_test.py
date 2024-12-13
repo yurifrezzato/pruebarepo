5212a37c4d4461b26a059382b2b8265b40c382e4
Yuri Frezzato Espinosa <yurifrezzato@gmail.com>
2024-12-13 19:48:43 +0100

    solve tests issues

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -32,7 +32,7 @@ class TestApi(unittest.TestCase):
             response.status, http.client.OK, f"Error en la petición API a {url}"
         )
         self.assertEqual(
-            response.read().decode(), "3", "ERROR ADD"
+            response.read().decode(), "2", "ERROR ADD"
         )
     
     def test_api_divide(self):
@@ -42,7 +42,7 @@ class TestApi(unittest.TestCase):
             response.status, http.client.OK, f"Error en la petición API a {url}"
         )
         self.assertEqual(
-            response.read().decode(), "3", "ERROR ADD"
+            response.read().decode(), "0.5", "ERROR ADD"
         )
 
     def test_api_sqrt(self):
