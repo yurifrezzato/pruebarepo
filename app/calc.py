a98b7f6117967242ec8c1843ffd66cccc76c92c1
Yuri Frezzato Espinosa <yurifrezzato@gmail.com>
2024-12-13 19:05:19 +0100

    fix indent

--- app/calc.py
+++ app/calc.py
@@ -21,8 +21,7 @@ class Calculator:
     def divide(self, x, y):
         self.check_types(x, y)
         if y == 0:
-            raise                                                TypeError("Division by zero is not possible")
-
+            raise TypeError("Division by zero is not possible")
         return x / y
 
     def power(self, x, y):
@@ -30,7 +29,7 @@ class Calculator:
         return x ** y
 
     def check_types(self, x, y):
-        if not isinstance(x, (int, float)) or                                                not isinstance(y, (int, float)):
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
             raise TypeError("Parameters must be numbers")
 
 
