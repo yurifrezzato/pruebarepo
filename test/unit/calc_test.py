80a81c22adfd30750f443fc65208296b9bf1de62
Yuri Frezzato Espinosa <yurifrezzato@gmail.com>
2025-01-28 18:51:35 +0100

    test solve division

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -19,13 +19,14 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(6, self.calc.add(3, 3))
         self.assertEqual(0, self.calc.add(2, -2))
         self.assertEqual(0, self.calc.add(-2, 2))
-        self.assertEqual(1, self.calc.add(1, 0))      
-        
+        self.assertEqual(1, self.calc.add(1, 0))
+
     def test_divide_method_returns_correct_result(self):
         self.assertEqual(1, self.calc.divide(2, 2))
         self.assertEqual(1.5, self.calc.divide(3, 2))
         self.assertRaises(TypeError, self.calc.divide, "2", 2)
-  
+        self.assertEqual(TypeError, self.calc.divide(2, 0))
+
     def test_add_method_fails_with_nan_parameter(self):
         self.assertRaises(TypeError, self.calc.add, "2", 2)
         self.assertRaises(TypeError, self.calc.add, 2, "2")
@@ -34,7 +35,7 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.add, 2, None)
         self.assertRaises(TypeError, self.calc.add, object(), 2)
         self.assertRaises(TypeError, self.calc.add, 2, object())
-    
+
     def test_divide_method_fails_with_nan_parameter(self):
         self.assertRaises(TypeError, self.calc.divide, "2", 2)
         self.assertRaises(TypeError, self.calc.divide, 2, "2")
@@ -46,14 +47,14 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(0, self.calc.multiply(-1, 0))
         self.assertEqual(-2, self.calc.multiply(-1, 2))
         self.assertRaises(TypeError, self.calc.multiply, "0", 0)
-        
+
     def test_power_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.power(2, 2))
         self.assertEqual(1, self.calc.power(1, 0))
         self.assertEqual(1, self.calc.power(-1, 0))
         self.assertEqual(-27, self.calc.power(-3, 3))
         self.assertRaises(TypeError, self.calc.power, "0", 0)
-        
+
     def test_substract_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.substract(10, 6))
         self.assertEqual(-2, self.calc.substract(256, 258))
@@ -61,6 +62,6 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(0, self.calc.substract(0, 0))
         self.assertEqual(0, self.calc.substract(0, 0))
         self.assertRaises(TypeError, self.calc.substract, "0", 0)
-        
+
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
