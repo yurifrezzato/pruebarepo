b238ea47018e10109cf2c591930d97c5d6133867
Yuri Frezzato Espinosa <yurifrezzato@gmail.com>
2025-02-01 19:57:50 +0100

    test cobertura

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -24,7 +24,7 @@ class TestCalculate(unittest.TestCase):
     def test_divide_method_returns_correct_result(self):
         self.assertEqual(1, self.calc.divide(2, 2))
         self.assertEqual(1.5, self.calc.divide(3, 2))
-        self.assertEqual(0, self.calc.divide(2,0))
+        self.asserRaises(TypeError, self.calc.divide(2,0))
         self.assertRaises(TypeError, self.calc.divide, "2", 2)
 
     def test_add_method_fails_with_nan_parameter(self):
